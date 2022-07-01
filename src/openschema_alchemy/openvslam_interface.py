3fbdef97feee79f2bec536a3c237a78e80f72e29
Marco Wallner <marco.wallner@ait.ac.at>
2022-07-01 11:20:18 +0200

    [WIP] added landmarks from openVSLAM

--- src/openschema_alchemy/openvslam_interface.py
+++ src/openschema_alchemy/openvslam_interface.py
@@ -15,15 +15,15 @@ from openschema_alchemy.model import *
 from datetime import datetime
 from pathlib import Path
 
-msg_pack_file_path = Path("/workspaces/openschema_postgres_utils/data/knapp_2022_03_03_dobl_test_env.msg")
+msg_pack_file_path = Path(
+    "/workspaces/openschema_postgres_utils/data/knapp_2022_03_03_dobl_test_env.msg")
 
 with open(msg_pack_file_path, "rb") as data_file:
-    loaded_data = msgpack.unpackb(data_file.read(), use_list=False, raw=False)
+    loaded_data = msgpack.unpackb(data_file.read(), use_list=True, raw=False)
     # print(loaded_data)
 
-
     engine = create_engine(
-    'postgresql://postgres:postgres@localhost:5432/postgres_alchemy_ait', echo=True)
+        'postgresql://postgres:postgres@localhost:5432/postgres_alchemy_ait', echo=True)
     if not engine.dialect.has_schema(engine, 'public'):
         engine.execute(CreateSchema('public'))
     Session = sessionmaker(bind=engine)
@@ -32,35 +32,51 @@ with open(msg_pack_file_path, "rb") as data_file:
 
     cameras = list(loaded_data['cameras'].keys())
     if len(cameras) != 1:
-        print(f"Only setups with one (stereo) camera supportet at the moment, given: {len(cameras)}")
+        print(
+            f"Only setups with one (stereo) camera supportet at the moment, given: {len(cameras)}")
         exit
 
     camera_name = list(loaded_data["cameras"].keys())[0]
 
-    sensor_rig = SensorRig(name=camera_name+" Rig", description={'T': [0.0]*12})
-    camera = Camera(name=camera_name, description={"openVSLAM_config": loaded_data["cameras"][camera_name]}, sensor_rig=sensor_rig)
+    sensor_rig = SensorRig(name=camera_name+" Rig",
+                           description={'T': [0.0]*12})
+    camera = Camera(name=camera_name, description={
+                    "openVSLAM_config": loaded_data["cameras"][camera_name]}, sensor_rig=sensor_rig)
 
     ts = datetime.utcnow()
-    newmap = Map(name=msg_pack_file_path.name + str(ts), description={"T_global": [0.0]*12}, created_at=ts, updated_at=ts)
+    newmap = Map(name=msg_pack_file_path.name + str(ts),
+                 description={"T_global": [0.0]*12}, created_at=ts, updated_at=ts)
 
-    pg = PoseGraph(name="SLAM graph", description={"Some_generic_setting": 0.2})
+    pg = PoseGraph(name="SLAM graph", description={
+                   "Some_generic_setting": 0.2})
     sensor_rig.posegraph = pg
-    num_vertices = 500
 
-    pg.map = newmap
+    landmarks = []
+    # Add landmarks
+    for landmark_key in list(loaded_data['landmarks'].keys()):
+        landmark_msgpack = loaded_data['landmarks'][landmark_key]
+        landmakr_pos_w = landmark_msgpack['pos_w']
+        lm = Landmark(
+            position=f"POINTZ({landmakr_pos_w[0]} {landmakr_pos_w[1]} {landmakr_pos_w[2]})")
+        landmarks.append(lm)
 
-    poses = [Pose(position=f"POINTZ({2*i} {3*i} {i})", posegraph=pg) for i in range(num_vertices)]
-    for i in poses[1:]:
-        i.parent = poses[0]
+    # for _, landmarkr_msgpack in loaded_data['landmarks']:
+    #     print(landmarkr_msgpack)
 
-    # TODO: Observations
-    # TODO: Landmarks
-    # TODO: Semantic info
+    # num_vertices = 500
+
+    # pg.map = newmap
+
+    # poses = [Pose(position=f"POINTZ({2*i} {3*i} {i})", posegraph=pg) for i in range(num_vertices)]
+    # for i in poses[1:]:
+    #     i.parent = poses[0]
+
+    # # TODO: Observations
+    # # TODO: Landmarks
+    # # TODO: Semantic info
 
     session = Session()
     session.add_all([newmap, pg, sensor_rig, camera] + poses)
     session.commit()
 
     session.commit()
-
-
