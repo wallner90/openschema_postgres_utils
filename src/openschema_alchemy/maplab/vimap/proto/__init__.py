050f9fe0ac80e396be5bd141817a9d17c8107cc8
Manuel Graef <manuel.graef@iviso.at>
2022-12-23 15:59:28 +0100

    [Maplab] First version of protocol buffer importer.

--- src/openschema_alchemy/maplab/vimap/proto/__init__.py
+++ src/openschema_alchemy/maplab/vimap/proto/__init__.py
@@ -1,5 +1,6 @@
 import gzip
 import os
+import yaml
 from glob import glob
 
 from maplab.vimap.proto import vi_map_pb2
@@ -13,15 +14,17 @@ def merge_vimap_file(message: vi_map_pb2.VIMap, file_name: str, compressed: bool
     message.MergeFromString(data)
 
 
-class VImap:
+class VIMap:
     def __init__(self, map_dir: str, compressed=True):
         assert 'vi_map' in os.listdir(map_dir), f"Cannot find 'vi_map' directory in path {map_dir}"
         vimap_dir = map_dir + '/vi_map'
-        compressed = compressed
 
-        print("VImap: loading data")
+        print("VIMap: loading data")
         self.message = vi_map_pb2.VIMap()
         merge_vimap_file(self.message, vimap_dir + '/missions', compressed)
         merge_vimap_file(self.message, vimap_dir + '/landmark_index', compressed)
         for file in glob(vimap_dir + '/edges*') + glob(vimap_dir + '/vertices*'):
             merge_vimap_file(self.message, file, compressed)
+
+        with open(vimap_dir + '/sensors.yaml') as file:
+            self.sensors = yaml.safe_load(file.read())
