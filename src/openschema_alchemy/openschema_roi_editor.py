bb71d00978699d8b924f633f68b6ae1b6092568d
Marco Wallner <marco.wallner@ait.ac.at>
2023-03-23 12:51:29 +0000

    added map and posegraph for sematic rois

--- src/openschema_alchemy/openschema_roi_editor.py
+++ src/openschema_alchemy/openschema_roi_editor.py
@@ -5,6 +5,8 @@ from sqlalchemy.orm import sessionmaker
 from rasterio.io import MemoryFile
 from rasterio.plot import show
 
+from datetime import datetime
+
 from sqlalchemy.dialects.postgresql import JSON
 
 from model import *
@@ -23,26 +25,43 @@ def main():
     for obj in session.query(Landmark).filter(text("descriptor->>'type' = 'pile'")).all():
         piles.setdefault(obj.descriptor['pile_nr'], []).append(obj)
 
+    ts = datetime.utcnow()
+    map = Map(name='sem_polygon_map',
+              description={"T_global": [
+                  0.0]*12, "AuthorithyID": "EPSG:25833", "CRS": "ETRS89 / UTM zone 33N"},
+              created_at=ts, updated_at=ts)
+    pg = PoseGraph(name="Virtual roi pose graph", map=map,
+                   description={})
+    # all (virtual) keypoints share the same (virtual) pose for semantic_observation
+    pose = Pose(position=f"POINTZ({0} {0} {0})", posegraph=pg)
+    semantic_observation = SemanticObservation(
+        pose=pose, algorithm="qgis_points", created_at=ts, updated_at=ts,
+        algorithm_settings={"qgis_points":
+                            {"test": 123,
+                             }})
+
     connections = []
     sem_geoms = []
-    
+    kps = []
+
     for nr, pile in piles.items():
-        # sem_geom = SemanticPolygon(description = {'type': 'pile', 'pile_nr': nr, 'description': 'A pile in in Asten.'})
-        sem_geom = SemanticPolygon(type=SemanticGeometryType.Polygon.value, description = {'type': 'pile', 'pile_nr': nr, 'description': 'A pile in in Asten.'})
+        sem_geom = SemanticPolygon(type=SemanticGeometryType.Polygon.value, description={
+                                   'type': 'pile', 'pile_nr': nr, 'description': 'A pile in Kieswerk Asten.'})
         sem_geoms.append(sem_geom)
         for lm in pile:
+            kps.append(SemanticKeypoint(observation = semantic_observation, landmark = lm))
             connection = ManyLandmarkHasManySemanticGeometry(
-                landmark = lm,
-                order_idx = lm.descriptor['corner_id']
+                landmark=lm,
+                order_idx=lm.descriptor['corner_id']
             )
             sem_geom.landmarks.append(connection)
             connections.append(connection)
             print("test")
         # create semantic geometry
-    
-    session.add_all(connections + sem_geoms)
-    session.commit()
 
+    session.add_all(connections + sem_geoms + kps +
+                    [map, pg, pose, semantic_observation])
+    session.commit()
 
     print("test")
 
